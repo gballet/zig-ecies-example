fa7fee44588d04e9f35da4599a38a85a281e9987
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2021-04-30 18:59:23 +0200

    cleanup by moving code into methods

--- src/main.zig
+++ src/main.zig
@@ -11,102 +11,126 @@ const builtin = std.builtin;
 const aes = crypto.core.aes;
 const hmac = crypto.auth.hmac;
 
-fn generate_key(key: *?*openssl.EC_KEY, skey: *?*const openssl.BIGNUM) !void {
-    std.log.info("NID: {}", .{openssl.NID_secp256k1});
-    key.* = openssl.EC_KEY_new_by_curve_name(openssl.NID_secp256k1);
-    if (key.* == null) {
-        std.log.info("could not create key: {}", .{openssl.ERR_get_error()});
-        return error.CouldNotCreateKey;
-    }
-    errdefer openssl.EC_KEY_free(key.*);
+const Key = struct {
+    key: ?*openssl.EC_KEY,
+    skey: ?*const openssl.BIGNUM,
 
-    if (openssl.EC_KEY_generate_key(key.*) != 1) {
-        std.log.info("could not generate random key: {}", .{openssl.ERR_get_error()});
-        return error.CouldNotGenerateKey;
-    }
+    // Generate a new key
+    pub fn generate() !Key {
+        var this = Key{ .key = null, .skey = null };
 
-    if (openssl.EC_KEY_check_key(key.*) != 1) {
-        return error.GeneratedKeyIsNotValid;
-    }
+        std.log.info("NID: {}", .{openssl.NID_secp256k1});
+        this.key = openssl.EC_KEY_new_by_curve_name(openssl.NID_secp256k1);
+        if (this.key == null) {
+            std.log.info("could not create key: {}", .{openssl.ERR_get_error()});
+            return error.CouldNotCreateKey;
+        }
+        errdefer openssl.EC_KEY_free(this.key.?);
 
-    skey.* = openssl.EC_KEY_get0_private_key(key.*);
-    if (skey.* == null) {
-        std.log.info("could not get private key: {}", .{openssl.ERR_get_error()});
-        return error.CouldNotGetPrivateKey;
-    }
-    std.log.info("secret key: {}", .{@ptrCast([*:0]u8, openssl.BN_bn2hex(skey.*))});
-}
+        if (openssl.EC_KEY_generate_key(this.key.?) != 1) {
+            std.log.info("could not generate random key: {}", .{openssl.ERR_get_error()});
+            return error.CouldNotGenerateKey;
+        }
 
-fn get_pkey(key: ?*openssl.EC_KEY) !*const openssl.EC_POINT {
-    var pkey = openssl.EC_KEY_get0_public_key(key);
-    if (pkey == null) {
-        std.log.info("could not get public key: {}", .{openssl.ERR_get_error()});
-        return error.CouldNotGetPublicKey;
-    }
-    return pkey.?;
-}
+        if (openssl.EC_KEY_check_key(this.key.?) != 1) {
+            return error.GeneratedKeyIsNotValid;
+        }
 
-fn get_bobs_key() !*openssl.EC_KEY {
-    var keybob = openssl.EC_KEY_new_by_curve_name(openssl.NID_secp256k1);
-    if (keybob == null) {
-        std.log.info("could not create Bob's public key: {}", .{@ptrCast([*:0]const u8, openssl.ERR_reason_error_string(openssl.ERR_get_error()))});
-        return error.CouldNotCreateBobsPublicKey;
-    }
-    errdefer openssl.EC_KEY_free(keybob);
-
-    var x_bob = openssl.BN_new();
-    if (openssl.BN_hex2bn(&x_bob, "ed5253cbb93b61511c27b3417d063843b9ccdd884ed00944ff3d10d1abc3b794") == 1) {
-        return error.InvalidXCoordinate;
-    }
-    defer openssl.BN_clear_free(x_bob);
-    var y_bob = openssl.BN_new();
-    if (openssl.BN_hex2bn(&y_bob, "98de555c716c1683e82107c990c7cc9fbf80c68ca8e82c25b34cf0d267d1383e") == 1) {
-        return error.InvalidYCoordinate;
-    }
-    defer openssl.BN_clear_free(y_bob);
-    if (openssl.EC_KEY_set_public_key_affine_coordinates(keybob, x_bob, y_bob) == 0) {
-        std.log.info("could not set Bob's public key: {}", .{@ptrCast([*:0]const u8, openssl.ERR_reason_error_string(openssl.ERR_get_error()))});
-        return error.CouldNotSetBobsPublicKey;
+        this.skey = openssl.EC_KEY_get0_private_key(this.key);
+        if (this.skey == null) {
+            std.log.info("could not get private key: {}", .{openssl.ERR_get_error()});
+            return error.CouldNotGetPrivateKey;
+        }
+        std.log.info("secret key: {}", .{@ptrCast([*:0]u8, openssl.BN_bn2hex(this.skey.?))});
+        return this;
     }
 
-    if (openssl.EC_KEY_check_key(keybob) != 1) {
-        return error.BobsKeyIsNotValid;
+    // a helper function to serialize the other party's key in these tests.
+    pub fn from_coordinates(x_str: [*c]const u8, y_str: [*c]const u8) !Key {
+        var newkey = openssl.EC_KEY_new_by_curve_name(openssl.NID_secp256k1);
+        if (newkey == null) {
+            std.log.info("could not create the public key: {}", .{@ptrCast([*:0]const u8, openssl.ERR_reason_error_string(openssl.ERR_get_error()))});
+            return error.CouldNotCreateBobsPublicKey;
+        }
+        errdefer openssl.EC_KEY_free(newkey);
+
+        var x_bob = openssl.BN_new();
+        if (openssl.BN_hex2bn(&x_bob, x_str) == 1) {
+            return error.InvalidXCoordinate;
+        }
+        defer openssl.BN_clear_free(x_bob);
+        var y_bob = openssl.BN_new();
+        if (openssl.BN_hex2bn(&y_bob, y_str) == 1) {
+            return error.InvalidYCoordinate;
+        }
+        defer openssl.BN_clear_free(y_bob);
+        if (openssl.EC_KEY_set_public_key_affine_coordinates(newkey, x_bob, y_bob) == 0) {
+            std.log.info("could not set public key: {}", .{@ptrCast([*:0]const u8, openssl.ERR_reason_error_string(openssl.ERR_get_error()))});
+            return error.CouldNotSetBobsPublicKey;
+        }
+
+        if (openssl.EC_KEY_check_key(newkey) != 1) {
+            return error.BobsKeyIsNotValid;
+        }
+
+        return Key{
+            .key = newkey,
+            .skey = null,
+        };
     }
 
-    return keybob.?;
-}
+    pub fn generate_shared(self: Key, other: Key) !*openssl.EC_POINT {
+        const grp = self.group();
 
-fn get_shared_secret(other: *const openssl.EC_POINT, skey: *openssl.BIGNUM, group: *const openssl.EC_GROUP) !*openssl.EC_POINT {
-    // S = Kb*r
-    var spoint = openssl.EC_POINT_new(group);
-    if (spoint == null) {
-        std.log.info("could not create r: {}", .{openssl.ERR_get_error()});
-        return error.CouldNotCreateS;
+        // S = Kb*r
+        var spoint = openssl.EC_POINT_new(grp.?);
+        if (spoint == null) {
+            std.log.info("could not create r: {}", .{openssl.ERR_get_error()});
+            return error.CouldNotCreateS;
+        }
+
+        var one = openssl.BN_new();
+        if (openssl.BN_one(one) != 1) {
+            return error.CouldNotSetOne;
+        }
+        var zero = openssl.BN_new();
+        if (openssl.BN_zero(zero) != 1) {
+            return error.CouldNotSetZero;
+        }
+
+        // S = 0*G + r*Kb
+        if (openssl.EC_POINT_mul(grp, spoint, one, try other.pubkey(), self.skey.?, null) != 1) {
+            std.log.info("could not compute S: {}", .{openssl.ERR_get_error()});
+            return error.CouldNotComputeS;
+        }
+        // check S != 0
+        if (openssl.EC_POINT_is_at_infinity(grp, spoint) == 1) {
+            return error.SAtInfinity;
+        }
+
+        return spoint.?;
     }
 
-    var one = openssl.BN_new();
-    if (openssl.BN_one(one) != 1) {
-        return error.CouldNotSetOne;
-    }
-    var zero = openssl.BN_new();
-    if (openssl.BN_zero(zero) != 1) {
-        return error.CouldNotSetZero;
-    }
-
-    // S = 0*G + r*Kb
-    if (openssl.EC_POINT_mul(group, spoint, one, other, skey, null) != 1) {
-        std.log.info("could not compute S: {}", .{openssl.ERR_get_error()});
-        return error.CouldNotComputeS;
-    }
-    // check S != 0
-    if (openssl.EC_POINT_is_at_infinity(group, spoint) == 1) {
-        return error.SAtInfinity;
+    pub fn pubkey(self: Key) !*const openssl.EC_POINT {
+        var pkey = openssl.EC_KEY_get0_public_key(self.key.?);
+        if (pkey == null) {
+            std.log.info("could not get public key: {}", .{openssl.ERR_get_error()});
+            return error.CouldNotGetPublicKey;
+        }
+        return pkey.?;
     }
 
-    return spoint.?;
-}
+    pub fn group(self: Key) ?*const openssl.EC_GROUP {
+        return openssl.EC_KEY_get0_group(self.key.?);
+    }
 
-fn get_x_coordinate(point: *openssl.EC_POINT, group: *const openssl.EC_GROUP, out: *[32]u8) !void {
+    pub fn free(self: Key) void {
+        openssl.EC_KEY_free(self.key.?);
+    }
+
+};
+
+fn get_x_coordinate(point: *const openssl.EC_POINT, group: *const openssl.EC_GROUP, out: *[32]u8) !void {
     var x = openssl.BN_new();
     defer openssl.BN_clear_free(x);
     if (openssl.EC_POINT_get_affine_coordinates_GFp(group, point, x, null, null) != 1) {
@@ -147,25 +171,19 @@ pub fn main() anyerror!void {
     defer openssl.OPENSSL_cleanup();
     std.log.info("initialized", .{});
 
-    var key: ?*openssl.EC_KEY = undefined;
-    var skey: ?*openssl.BIGNUM = undefined;
-    try generate_key(&key, &skey);
-    defer openssl.EC_KEY_free(key);
-
-    var pkey = try get_pkey(key);
+    const r = try Key.generate();
+    defer r.free();
 
     // Prepare Kb
-    var keybob = try get_bobs_key();
-    defer openssl.EC_KEY_free(keybob);
-    const bob_pkey = try get_pkey(keybob);
-    const group = openssl.EC_KEY_get0_group(keybob);
+    var keybob = try Key.from_coordinates("ed5253cbb93b61511c27b3417d063843b9ccdd884ed00944ff3d10d1abc3b794", "98de555c716c1683e82107c990c7cc9fbf80c68ca8e82c25b34cf0d267d1383e");
+    defer keybob.free();
 
     // Shared secret
-    const spoint = try get_shared_secret(bob_pkey, skey.?, group.?);
+    var spoint = try r.generate_shared(keybob);
     defer openssl.EC_POINT_clear_free(spoint);
 
     var s: [32]u8 = undefined;
-    try get_x_coordinate(spoint, group.?, &s);
+    try get_x_coordinate(spoint, keybob.group().?, &s);
 
     std.log.info("s={x}", .{s});
 
